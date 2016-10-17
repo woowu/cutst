efae7ed14184e4fec9703e5ddd517a2e81e5c960
Woody Wu <narkewoody@gmail.com>
2016-10-17 10:21:09 +0800

    run* scripts

--- rundlms.sh
+++ rundlms.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-t=0
 p=120
 while true; do
     now=`date +%s`
     t=`expr $now / $p \* $p`
     270 6 1 13 $t 1
-    sleep 1
+    sleep 30
 done
+
---
+++ rundlt645.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dlt645tst.py /dev/tts4 -tweisheng -d4 -s 37102083
+
