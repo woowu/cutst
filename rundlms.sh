f4765325d51e2f79e48219ca97b3f3762675e3d0
Woody Wu <narkewoody@gmail.com>
2016-10-17 13:02:15 +0800

    update run*.sh

--- rundlms.sh
+++ rundlms.sh
@@ -4,7 +4,7 @@ p=120
 while true; do
     now=`date +%s`
     t=`expr $now / $p \* $p`
-    270 6 1 13 $t 1
+    270 -L 6 1 13 $t 1
     sleep 30
 done
 
--- rundlt645.sh
+++ rundlt645.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-dlt645tst.py /dev/tts4 -tweisheng -d4 -s 37102083
+dlt645tst.py /dev/tts4 -n100000 -tweisheng -d4 -s 37102083
+dlt645tst.py /dev/tts4 -n100000 -tkeli 37102083
 
