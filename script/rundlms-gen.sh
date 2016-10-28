13543a64c16181d98daa2fd785172f89f759dbdf
Woody Wu <narkewoody@gmail.com>
2016-10-29 00:48:37 +0800

    tc #06

--- script/rundlms-gen.sh
+++ script/rundlms-gen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NODB=
+DEBUG=
 PERIOD=120
 
 meter_no=$1
@@ -14,12 +14,12 @@ if [[ ! $iterations -gt 0 ]]; then
     echo bad iterations
     exit 1
 fi
-if [[ $NODB != "yes" && ! -f /ffc/cfg/config.db ]]; then
+if [[ $DEBUG != "yes" && ! -f /ffc/cfg/config.db ]]; then
     echo no config
     exit 1
 fi
 
-if [[ $NODB != "yes" ]]; then
+if [[ $DEBUG != "yes" ]]; then
     com_no=`sqlite3 /ffc/cfg/config.db "select ncomid from meterunitconfig where nmeterid=$meter_no;"`
     baud_idx=`sqlite3 /ffc/cfg/config.db "select nbaudft from meterunitconfig where nmeterid=$meter_no;"`
 else
@@ -53,10 +53,15 @@ do_rd() {
         t="`date -d@$lp_to +"%Y-%m-%d %H:%M:%S"`"
         opts+=(--lp-to "${t}")
     fi
+    if [[ -n $lp_from || -n $lp_to ]]; then
+        opts+=(--no-lp-time-hist)
+    fi
     echo xdlms "${opts[@]}" \
         $com_no $meter_no $baud_idx $1 1
-    xdlms "${opts[@]}" \
-        $com_no $meter_no $baud_idx $1 1
+    if [[ $DEBUG != "yes" ]]; then
+        xdlms "${opts[@]}" \
+            $com_no $meter_no $baud_idx $1 1
+    fi
     if [[ $? -ne 0 ]]; then
         echo "ERR: xdlms error, exit code = $?"
         sleep 200 
@@ -71,21 +76,11 @@ while [[ $i -lt $iterations ]]; do
     acqtime=`expr $acqtime / $PERIOD \* $PERIOD`
     c=`expr $i % 10`
     case $c in 
-        [0-4])          # 50%, lp window = 2hr, no register
+        [0-4])
             do_rd $acqtime "no" `expr $acqtime - 7200 + 1` $acqtime
             ;;
-        [5-6])          # 20%, lp window = 0.5hr, with register
-            do_rd $acqtime "yes" `expr $acqtime - 1800 + 1` $acqtime
-            ;;
-        [7-9])          # 30%, random lp timewindow (<=0.5hr) 
-            lp_to=`expr $acqtime + 300`
-            d=`expr $RANDOM % 10 + 1`
-            d=`expr $d \* 60`
-            lp_to=`expr $lp_to - $d`
-            d=`expr $RANDOM % 30 + 1`
-            d=`expr $d \* 60`
-            lp_from=`expr $lp_to - $d + 1`
-            do_rd $acqtime "yes" $lp_from $lp_to
+        [5-9])
+            do_rd $acqtime "yes"
             ;;
     esac
     i=`expr $i + 1`
