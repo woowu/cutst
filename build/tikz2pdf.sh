ffd2c04d953511e641fc6e1f40f8d58356cd461e
Woody Wu <narkewoody@gmail.com>
2016-10-30 22:53:14 +0800

    script to create svg/png

---
+++ build/tikz2pdf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+tikz=$1
+ofile=$2
+
+cat >tmp.tex <<EOF
+\documentclass[border={2pt, 2pt, 0pt, 2pt}]{standalone}
+\usepackage{graphics}
+\usepackage{tikz}
+\usetikzlibrary{arrows.meta, shadows, shapes, fit, positioning, calc, shadows}
+\tikzset{help lines/.style={very thine, gray}}
+
+\begin{document}
+\input{$tikz}
+\end{document}
+EOF
+
+pdflatex tmp.tex
+mv tmp.pdf $ofile
+rm tmp.tex tmp.aux tmp.log
---
+++ build/tikz2svg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+tikz=$1
+ofile=$2
+
+cat >tmp.tex <<EOF
+\documentclass[border={2pt, 2pt, 0pt, 2pt}]{standalone}
+\usepackage{graphics}
+\usepackage{tikz}
+\usetikzlibrary{arrows.meta, shadows, shapes, fit, positioning, calc, shadows}
+\tikzset{help lines/.style={very thine, gray}}
+
+\begin{document}
+\input{$tikz}
+\end{document}
+EOF
+
+pdflatex tmp.tex
+pdf2svg tmp.pdf $ofile
+rm tmp.pdf tmp.tex tmp.aux tmp.log
+
