47e2887136e8814abc5669ce01615f8087f0053e
Woody Wu <narkewoody@gmail.com>
2016-11-10 22:55:40 +0800

    fix testcase-07

--- script/testcase-07.1.sh
+++ script/testcase-07.1.sh
@@ -8,9 +8,10 @@ fi
 [ "$CUTST_LOGSERVER" != "" ] || CUTST_LOGSERVER=10.86.201.53
 
 killall 00
+syslog -m start
 sleep 5
 while [[ $# -gt 0 ]]; do
-    rundlms-gen.sh $1 $iterations >/dev/null 2>&1 &
+    rundlms-gen.sh $1 $iterations 2>&1 | logger &
     shift
 done
 
--- script/testcase-07.2.sh
+++ script/testcase-07.2.sh
@@ -8,9 +8,10 @@ fi
 [ "$CUTST_LOGSERVER" != "" ] || CUTST_LOGSERVER=10.86.201.53
 
 killall 00
+syslog -m start
 sleep 5
 while [[ $# -gt 0 ]]; do
-    rundlt645.sh /dev/tts$(($1 - 1)) $2 weisheng $iterations >/dev/null 2>&1 &
+    rundlt645.sh /dev/tts$(($1 - 1)) $2 weisheng $iterations 2>&1 | logger &
     shift
     shift
 done
