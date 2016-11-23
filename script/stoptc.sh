df94763d35b1b1f41406fba254782023b6805a94
Woody Wu <narkewoody@gmail.com>
2016-11-23 10:02:47 +0100

    fix and add scripts

--- script/stoptc.sh
+++ script/stoptc.sh
@@ -2,7 +2,7 @@
 
 echo "stop dlt645tst"
 for pid in \
-    `ps -Cpython -opid,command |grep dlt645tst.py |sed -n 's/^\s\+\([0-9]\+\)\s\+python.*$/\1/p'`; do
+    `ps -Cpython -opid,command |grep dlt645tst.py |grep -v grep |sed -n 's/^\s*\([0-9]\+\)\s\+python.*$/\1/p'`; do
     echo killing $pid
     kill $pid
 done
--- script/testcase-09.sh
+++ script/testcase-09.sh
@@ -6,6 +6,7 @@ N_ITERATIONS=50000
 # CU+ZQ(H02): Dl645 + Dlms via CU
 #
 xdlms2 --allow-event-acq --allow-sync-clock --default-intvl 2 --loops $N_ITERATIONS \
+    --aa-lifetime 30 \
     /dev/tts6 7615 21234567 9600 2 | nc $LOG_SERVER 2120 &
 rundlt645.sh /dev/tts7 85786615 weisheng $N_ITERATIONS | nc $LOG_SERVER 2121 &
 
