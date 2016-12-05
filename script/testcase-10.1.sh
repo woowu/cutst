6d1293de3c9904fcef41e8dadb75584348f1b549
Woody Wu <narkewoody@gmail.com>
2016-12-05 11:44:35 +0100

    testcase-10: use fifo

--- script/testcase-10.1.sh
+++ script/testcase-10.1.sh
@@ -3,20 +3,62 @@
 usage="$(basename "$0") iteration_nr com_port hdlc_addr \
 profile_start_time profile_end_time"
 
+FIFO=/dev/shm/$(basename "$0").fifo
+LOG=/dev/shm/$(basename "$0").log
+
+iterations=$1
+dev=/dev/tts$(($2 - 1))
+hdlc_addr=$3
+profile_start="$4"
+profile_end="$5"
+
+test_prg_pid=
+
+function on_exit {
+    echo exiting
+    if [ -n $test_prg_pid ]; then
+        kill $test_prg_pid
+    fi
+    rm -f $FIFO
+    exit 
+}
+
+function start_test_prg {
+    echo "to start test program"
+    xdlms2 --brief --no-reg --loops $iterations --aa-lifetime 120 \
+        --lp-read-sliding-window \
+        --lp-sliding-window-start "$profile_start" \
+        --lp-sliding-window-end "$profile_end" \
+        --lp-sliding-window-width 1 \
+        --lp-sliding-window-step 900 \
+        $dev $hdlc_addr 21234567 9600 2 \
+        >&3 2>&3 &
+    test_prg_pid=$!
+    echo "test process $test_prg_pid started in background"
+}
+
 if [ "$1" == "-h" ]; then
     echo "$usage"
     exit
 fi
 
-syslog -m start
-sleep 2
+trap on_exit SIGTERM SIGINT
 
-xdlms2 --brief --no-reg --loops $1 --aa-lifetime 120 \
-    --lp-read-sliding-window \
-    --lp-sliding-window-start "$4" \
-    --lp-sliding-window-end "$5" \
-    --lp-sliding-window-width 1 \
-    --lp-sliding-window-step 900 \
-    /dev/tts$(($2 - 1)) $3 21234567 9600 2 \
-    2>&1 | cut -d' ' -f2- | logger --id &
+if [ ! -p $FIFO ]; then
+    mkfifo $FIFO
+fi
+
+exec 3<>$FIFO
+start_test_prg
+sleep 1
+echo "output is senting to $LOG"
+
+while true; do
+    read line <&3
+    echo "$line" >>$LOG
+    sz=$(stat -c '%s' $LOG)
+    if [ $sz -gt 2097152 ]; then
+        rm -f $LOG
+    fi
+done
 
--- script/testcase-10.2.sh
+++ script/testcase-10.2.sh
@@ -2,14 +2,54 @@
 
 usage="$(basename "$0") iteration_nr com_port meter_no"
 
+FIFO=/dev/shm/$(basename "$0").fifo
+LOG=/dev/shm/$(basename "$0").log
+
+iterations=$1
+dev=/dev/tts$(($2 - 1))
+meter_no=$3
+
+test_prg_pid=
+
+function on_exit {
+    echo exiting
+    if [ -n $test_prg_pid ]; then
+        kill $test_prg_pid
+    fi
+    rm -f $FIFO
+    exit 
+}
+
+function start_test_prg {
+    echo "to start test program"
+    dlt645tst.py $dev -n$iterations -tsky -d4 -s $meter_no \
+        >&3 2>&3 &
+    test_prg_pid=$!
+    echo "test process $test_prg_pid started in background"
+}
+
 if [ "$1" == "-h" ]; then
     echo "$usage"
     exit
 fi
 
-syslog -m start
-sleep 2
+trap on_exit SIGTERM SIGINT
 
-rundlt645.sh /dev/tts$(($2 - 1)) $3 sky $1 \
-    2>&1 | cut -d' ' -f3- | logger --id &
+if [ ! -p $FIFO ]; then
+    mkfifo $FIFO
+fi
+
+exec 3<>$FIFO
+start_test_prg
+sleep 1
+echo "output is senting to $LOG"
+
+while true; do
+    read line <&3
+    echo "$line" >>$LOG
+    sz=$(stat -c '%s' $LOG)
+    if [ $sz -gt 2097152 ]; then
+        rm -f $LOG
+    fi
+done
 
